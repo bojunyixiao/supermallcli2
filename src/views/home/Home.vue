10cbf98f060f5c2388fd069c57f010b5055e6b57
liubojun <liubojun@embed-tec.com>
2021-04-06 14:23:22 +0800

    下部导航栏

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,5 +1,9 @@
 <template>
-  <h2>首页</h2>
+  <div class="home-head">
+    <div class="home-left"></div>
+    <div class="home-center">首页</div>
+    <div class="home-right"></div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +13,22 @@
 </script>
 
 <style scoped>
+  .home-head{
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+    box-shadow: 0 1px 1px rgba(100,100,100,.1);
+  }
+  .home-left, .home-right{
+    width: 50px;
+  }
+  .home-center{
+    position: fixed;
+    top:0px;
+    left: 0px;
+    right: 0px;
 
+    flex: 1;
+  }
 </style>
